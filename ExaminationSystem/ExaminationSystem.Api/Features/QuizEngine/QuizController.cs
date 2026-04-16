70bcf5ead8095798f3d1388fe2342396a1ed0c72
MahmoudAmin5 <ma7moud888.amim@gmail.com>
2026-04-16 11:57:40 +0200

    Create QuizController.cs

---
+++ ExaminationSystem/ExaminationSystem.Api/Features/QuizEngine/QuizController.cs
@@ -0,0 +1,41 @@
+﻿using ExaminationSystem.Api.Extensions;
+using ExaminationSystem.Api.Features.QuizEngine.StartQuiz;
+using ExaminationSystem.Api.Features.QuizEngine.StartQuiz.Dtos;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace ExaminationSystem.Api.Features.QuizEngine
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuizController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public QuizController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("{quizId:guid}/start")]
+        [ProducesResponseType(typeof(StartQuizResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        public async Task<IActionResult> StartQuiz([FromRoute] Guid quizId, CancellationToken cancellationToken)
+        {
+            var studentId = Guid.Parse(
+                User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+
+            var result = await _mediator.Send(
+                new StartQuizCommand(quizId, studentId), cancellationToken);
+
+            if (result.IsFailure)
+                return result.ToActionResult();
+
+            return result.ToActionResult();
+        }
+    }
+}
