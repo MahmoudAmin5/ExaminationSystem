ffe6ca5ab652a3d8e9cb88d1a27f02bfb341c0c7
MahmoudAmin5 <ma7moud888.amim@gmail.com>
2026-04-16 11:53:10 +0200

    SaveShuffledQuestionsOrderCommand

---
+++ ExaminationSystem/ExaminationSystem.Api/Features/QuizEngine/StartQuiz/Dtos/ShuffleResult.cs
@@ -0,0 +1,7 @@
+﻿namespace ExaminationSystem.Api.Features.QuizEngine.StartQuiz.Dtos
+{
+    public record ShuffleResult
+    {
+        public List<ShuffledQuestionDto> Questions { get; init; } = [];
+    }
+}
