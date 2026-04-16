54f605470db46b9efe15501ebcb5b2d3625dab2d
MahmoudAmin5 <ma7moud888.amim@gmail.com>
2026-04-16 11:51:31 +0200

    GetInProgressStudentAttemptQuery

---
+++ ExaminationSystem/ExaminationSystem.Api/Features/QuizEngine/StartQuiz/Dtos/QuizAttemptDto.cs
@@ -0,0 +1,15 @@
+﻿using ExaminationSystem.Api.Domain.Enums;
+
+namespace ExaminationSystem.Api.Features.QuizEngine.StartQuiz.Dtos
+{
+    public class QuizAttemptDto
+    {
+        public Guid AttemptId { get; set; }
+        public Guid StudentId { get; set; }
+        public Guid QuizId { get; set; }
+        public AttemptStatus Status { get; set; }
+        public DateTime StartedAt {  get; set; } 
+        public DateTime Deadline { get; set; }
+
+    }
+}
---
+++ ExaminationSystem/ExaminationSystem.Api/Features/QuizEngine/StartQuiz/Queries/GetInProgressStudentAttemptQuery.cs
@@ -0,0 +1,40 @@
+﻿using ExaminationSystem.Api.Domain.Contracts.Repository.Contract;
+using ExaminationSystem.Api.Domain.Entities.Data;
+using ExaminationSystem.Api.Domain.Enums;
+using ExaminationSystem.Api.Features.QuizEngine.StartQuiz.Dtos;
+using ExaminationSystem.Api.Shared.Results;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExaminationSystem.Api.Features.QuizEngine.StartQuiz.Queries
+{
+    public record GetInProgressStudentAttemptQuery(Guid StudentId, Guid QuizId) : IRequest<Result<QuizAttemptDto>>;
+    public class GetInProgressStudentAttemptQueryHandler : IRequestHandler<GetInProgressStudentAttemptQuery, Result<QuizAttemptDto>>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public GetInProgressStudentAttemptQueryHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<Result<QuizAttemptDto>> Handle(GetInProgressStudentAttemptQuery request, CancellationToken cancellationToken)
+        {
+            var inProgressAttempt = await _unitOfWork.Repository<QuizAttempt, Guid>()
+                .AsNoTracking()
+                .FirstOrDefaultAsync(a => a.StudentId == request.StudentId && a.QuizId == request.QuizId && a.Status == AttemptStatus.InProgress, cancellationToken);
+            
+            if (inProgressAttempt == null) return Result<QuizAttemptDto>.Failure(Error.NotFound("In-ProgressAttempts.NotFound","In-progress attempt not found"));
+            
+            var response = new QuizAttemptDto
+            {
+                AttemptId = inProgressAttempt.Id,
+                Status = inProgressAttempt.Status,
+                StartedAt = inProgressAttempt.StartedAt,
+                Deadline = inProgressAttempt.Deadline
+
+            };
+
+            return Result<QuizAttemptDto>.Success(response);
+        }
+    }
+}
