7d57cd55ae747c5673cbb4fa524129e4f298710a
MahmoudAmin5 <ma7moud888.amim@gmail.com>
2026-04-18 13:41:01 +0200

    add GetAttemptWithQuizQuery

---
+++ ExaminationSystem/ExaminationSystem.Api/Features/QuizEngine/ViewResult/Dtos/AttemptDto.cs
@@ -0,0 +1,14 @@
+﻿namespace ExaminationSystem.Api.Features.QuizEngine.ViewResult.Dtos
+{
+    public class AttemptDto
+    {
+        public Guid Id { get; init; }
+        public Guid QuizId { get; init; }
+        public Guid StudentId { get; init; }
+        public string Status { get; init; } = string.Empty;
+        public decimal? Score { get; init; }
+        public bool? Passed { get; init; }
+        public int? TotalQuestions { get; init; }
+        public DateTime? SubmittedAt { get; init; }
+    }
+}
