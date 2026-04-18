749521eb909284293398508e882c226166bfd38f
MahmoudAmin5 <ma7moud888.amim@gmail.com>
2026-04-18 09:27:02 +0200

    add AutoSubmitExpiredAttemptCommand

---
+++ ExaminationSystem/ExaminationSystem.Api/Features/QuizEngine/TimerHandling/Dtos/AutoSubmitDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ExaminationSystem.Api.Features.QuizEngine.TimerHandling.Dtos
+{
+    public class AutoSubmitDto
+    {
+        public Guid AttemptId { get; set; }
+        public DateTime SubmittedAt { get; set; }
+        public bool WasAutoSubmitted { get; set; }
+        public decimal? Score { get; set; }
+        public bool IsPassed { get; set; }
+    }
+}
