7fd201ececf93556d2118bbfb98ac5d1dac2a5b3
Sara <saramaged660@gmail.com>
2026-04-18 06:55:09 +0200

    implement global query filters extension method

--- ExaminationSystem/ExaminationSystem.Api/Infrastructure/Persistence/ApplicationDbContext.cs
+++ ExaminationSystem/ExaminationSystem.Api/Infrastructure/Persistence/ApplicationDbContext.cs
@@ -43,15 +43,7 @@ namespace ExaminationSystem.Api.Infrastructure.Persistence
 
             DbSeeder.SeedData(modelBuilder);
 
-            modelBuilder.Entity<User>().HasQueryFilter(u => !u.IsDeleted);
-            modelBuilder.Entity<Diploma>().HasQueryFilter(d => !d.IsDeleted);
-            modelBuilder.Entity<Quiz>().HasQueryFilter(q => !q.IsDeleted);
-            modelBuilder.Entity<Question>().HasQueryFilter(q => !q.IsDeleted);
-            modelBuilder.Entity<AnswerOption>().HasQueryFilter(o => !o.IsDeleted);
-            modelBuilder.Entity<Enrollment>().HasQueryFilter(e => !e.IsDeleted);
-            modelBuilder.Entity<QuizAttempt>().HasQueryFilter(a => !a.IsDeleted);
-            modelBuilder.Entity<AttemptAnswer>().HasQueryFilter(a => !a.IsDeleted);
-            modelBuilder.Entity<OtpCode>().HasQueryFilter(o => !o.IsDeleted);
+           
 
         }
        
---
+++ ExaminationSystem/ExaminationSystem.Api/Infrastructure/Persistence/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,22 @@
+﻿using ExaminationSystem.Api.Domain.Entities.Account;
+using ExaminationSystem.Api.Domain.Entities.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExaminationSystem.Api.Infrastructure.Persistence.Extensions
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void ApplyGlobalQueryFilters(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasQueryFilter(u => !u.IsDeleted);
+            modelBuilder.Entity<Diploma>().HasQueryFilter(d => !d.IsDeleted);
+            modelBuilder.Entity<Quiz>().HasQueryFilter(q => !q.IsDeleted);
+            modelBuilder.Entity<Question>().HasQueryFilter(q => !q.IsDeleted);
+            modelBuilder.Entity<AnswerOption>().HasQueryFilter(o => !o.IsDeleted);
+            modelBuilder.Entity<Enrollment>().HasQueryFilter(e => !e.IsDeleted);
+            modelBuilder.Entity<QuizAttempt>().HasQueryFilter(a => !a.IsDeleted);
+            modelBuilder.Entity<AttemptAnswer>().HasQueryFilter(a => !a.IsDeleted);
+            modelBuilder.Entity<OtpCode>().HasQueryFilter(o => !o.IsDeleted);
+        }
+    }
+}
