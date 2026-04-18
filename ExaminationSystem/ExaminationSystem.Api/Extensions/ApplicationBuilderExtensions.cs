fd8376f0a48a857094e46924f3fbcf6128cf8f4a
Sara <saramaged660@gmail.com>
2026-04-18 08:15:40 +0200

    implement database initialization extension for automated migrations and seeding

---
+++ ExaminationSystem/ExaminationSystem.Api/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,35 @@
+﻿using ExaminationSystem.Api.Domain.Entities.Account;
+using ExaminationSystem.Api.Infrastructure.Persistence;
+using ExaminationSystem.Api.Infrastructure.Persistence.Seeding;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExaminationSystem.Api.Extensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static async Task InitializeDatabaseAsync(this IApplicationBuilder app)
+        {
+            using var scope = app.ApplicationServices.CreateScope();
+            var services = scope.ServiceProvider;
+
+            try
+            {
+                var context = services.GetRequiredService<ApplicationDbContext>();
+                var userManager = services.GetRequiredService<UserManager<User>>();
+                var roleManager = services.GetRequiredService<RoleManager<IdentityRole<Guid>>>();
+
+                 //Update-Database
+                await context.Database.MigrateAsync();
+
+                
+                await DbSeeder.SeedAsync(userManager, roleManager);
+            }
+            catch (Exception ex)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "An error occurred during database initialization.");
+            }
+        }
+    }
+}
