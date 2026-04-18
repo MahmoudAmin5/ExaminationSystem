6cab7cc373f1e7f7a5e241f1f39b792c15204820
Sara <saramaged660@gmail.com>
2026-04-18 15:09:22 +0200

    Implement GetDiplomasHandler

---
+++ ExaminationSystem/ExaminationSystem.Api/Features/Diplomas/Dtos/DiplomaDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ExaminationSystem.Api.Features.Diplomas.Dtos
+{
+    public record DiplomaDto(
+      Guid Id,
+    string Title,
+    string Description,
+     int QuizzesCount,
+    double ProgressPercentage);
+
+}
