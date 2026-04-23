0cc7d03baf76213fc6f5e1e13ce9616ce9a4ccba
MahmoudAmin5 <ma7moud888.amim@gmail.com>
2026-04-23 17:43:48 +0200

    add GetAttemptAnswerQuery

---
+++ ExaminationSystem/ExaminationSystem.Api/Features/QuizEngine/Shared/Dtos/AttemptAnswerDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ExaminationSystem.Api.Features.QuizEngine.Shared.Dtos
+{
+    public class AttemptAnswerDto
+    {
+        public Guid AttemptId { get; set; }
+        public Guid QuestionId { get; set; }
+        public Guid? SelectedOptionId { get; set; }
+        public DateTime AnsweredAt { get; set; }
+        public bool? IsCorrect { get; set; }
+    }
+}
---
+++ ExaminationSystem/ExaminationSystem.Api/Features/QuizEngine/Shared/Queries/GetAttemptAnswerQuery.cs
@@ -0,0 +1,38 @@
+﻿using ExaminationSystem.Api.Domain.Contracts.Repository.Contract;
+using ExaminationSystem.Api.Domain.Entities.Data;
+using ExaminationSystem.Api.Features.QuizEngine.Shared.Dtos;
+using ExaminationSystem.Api.Features.QuizEngine.ViewResult.Queries;
+using ExaminationSystem.Api.Shared.Results;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExaminationSystem.Api.Features.QuizEngine.Shared.Queries
+{
+    public record GetAttemptAnswerQuery(Guid AttemptId) : IRequest<Result<List<AttemptAnswerDto>>>;
+    public class GetAttemptAnswerHandler : IRequestHandler<GetAttemptAnswerQuery, Result<List<AttemptAnswerDto>>>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public GetAttemptAnswerHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<Result<List<AttemptAnswerDto>>> Handle(GetAttemptAnswerQuery request, CancellationToken cancellationToken)
+        {
+            var attemptAnswers = await _unitOfWork.Repository<AttemptAnswer,int>()
+                .AsNoTracking()
+                .Where(a => a.AttemptId == request.AttemptId)
+                .Select(a => new AttemptAnswerDto
+                {
+                    AttemptId = a.AttemptId,
+                    QuestionId = a.QuestionId,
+                    SelectedOptionId = a.SelectedOptionId,
+                    AnsweredAt = a.AnsweredAt,
+                    IsCorrect = a.IsCorrect ?? false
+                })
+                .ToListAsync(cancellationToken);
+
+            return Result<List<AttemptAnswerDto>>.Success(attemptAnswers);
+        }
+    }
+}
