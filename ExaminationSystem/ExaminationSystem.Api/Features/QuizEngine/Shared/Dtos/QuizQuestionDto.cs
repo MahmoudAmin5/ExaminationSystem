8442258011f9e34d94a6cbeb2399a3c144ff43cc
MahmoudAmin5 <ma7moud888.amim@gmail.com>
2026-04-23 17:44:41 +0200

    add :GetQuizByIdQuery

---
+++ ExaminationSystem/ExaminationSystem.Api/Features/QuizEngine/Shared/Dtos/QuizQuestionDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ExaminationSystem.Api.Features.QuizEngine.Shared.Dtos
+{
+    public class QuizQuestionDto
+    {
+        public Guid Id { get; set; }
+        public string Text { get; set; }
+         public string? Explanation { get; set; }
+    }
+}
