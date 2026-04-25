b0f367c3ffef5767ad91c599eaed4c34045abb53
MahmoudAmin5 <ma7moud888.amim@gmail.com>
2026-04-25 15:23:27 +0300

    add(Admin) : PublishQuizCommand

---
+++ ExaminationSystem/ExaminationSystem.Api/Features/AdminManagement/Publish&UnpublishQuiz/Validators/PublishQuizCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using ExaminationSystem.Api.Features.AdminManagement.Publish_UnpublishQuiz.Command;
+using FluentValidation;
+
+namespace ExaminationSystem.Api.Features.AdminManagement.Publish_UnpublishQuiz.Validators
+{
+    public class PublishQuizCommandValidator : AbstractValidator<PublishQuizCommand>
+    {
+        public PublishQuizCommandValidator()
+        {
+            RuleFor(x => x.QuizId)
+                .NotEmpty().WithMessage("Quiz ID is required.");
+        }
+    }
+
+}
---
+++ ExaminationSystem/ExaminationSystem.Api/Features/AdminManagement/Shard/Queries/GetQuestionCountForQuiz.cs
@@ -0,0 +1,22 @@
+﻿using ExaminationSystem.Api.Domain.Contracts.Repository.Contract;
+using ExaminationSystem.Api.Domain.Entities.Data;
+using MediatR;
+
+namespace ExaminationSystem.Api.Features.AdminManagement.Shard.Queries
+{
+    public record GetQuestionCountForQuiz(Guid quizId) : IRequest<int>;
+    public class GetQuestionCountForQuizHandler : IRequestHandler<GetQuestionCountForQuiz, int>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public GetQuestionCountForQuizHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<int> Handle(GetQuestionCountForQuiz request, CancellationToken cancellationToken)
+        {
+           return await _unitOfWork.Repository<Question,Guid>()
+                .CountAsync(q => q.QuizId == request.quizId, cancellationToken);
+        }
+    }
+
+}
