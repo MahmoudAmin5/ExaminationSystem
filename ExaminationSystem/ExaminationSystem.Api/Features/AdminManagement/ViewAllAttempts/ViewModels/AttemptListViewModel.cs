731d1f41172ea9837f1a41c81975bfae59f5ceb7
MahmoudAmin5 <ma7moud888.amim@gmail.com>
2026-04-25 20:33:27 +0300

    add(Admin): GetAllAttempts Query

---
+++ ExaminationSystem/ExaminationSystem.Api/Features/AdminManagement/ViewAllAttempts/ViewModels/AttemptListViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ExaminationSystem.Api.Features.AdminManagement.ViewAllAttempts.ViewModels
+{
+    public class AttemptListViewModel
+    {
+        public Guid AttemptId { get; set; }
+        public Guid StudentId { get; set; }
+        public string StudentName { get; set; } = string.Empty;
+        public string QuizTitle { get; set; } = string.Empty;
+        public decimal? Score { get; set; }
+        public string Status { get; set; } = string.Empty;
+        public DateTime? SubmittedAt { get; set; }
+    }
+}
